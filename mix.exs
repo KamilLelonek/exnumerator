49d624b3b83df5ce4d41be6a7258bc10afa6af1c
Kamil Lelonek <squixy.sln@gmail.com>
2016-01-09 20:13:14 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Exnumterator.Mixfile do
   def project do
     [
       app:             :exnumterator,
-      version:         "0.0.1",
-      elixir:          "~> 1.1",
+      version:         "1.0.0",
+      elixir:          "~> 1.2",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description:     description,
