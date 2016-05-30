62cd35212113111e7c9bdf41c993d6cb156c0b0d
Kamil Lelonek <squixy.sln@gmail.com>
2016-05-30 19:20:02 +0200

    Provide sample method to fetch a random value

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exnumerator.Mixfile do
   def project do
     [
       app:             :exnumerator,
-      version:         "1.0.0",
+      version:         "1.1.0",
       elixir:          "~> 1.2",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
--- test/exnumerable_test.exs
+++ test/exnumerable_test.exs
@@ -1,13 +1,18 @@
 defmodule ExnumeratorTest do
   use ExUnit.Case
 
+  @values [:sent, :read, :received, :delivered]
+
   defmodule Message do
     use Exnumerator,
       values: [:sent, :read, :received, :delivered]
   end
 
   test "should store given values",
-    do: assert Message.values == [:sent, :read, :received, :delivered]
+    do: assert Message.values == @values
+
+  test "should return a random value",
+    do: assert Message.sample in @values
 
   test "should augment given types" do
     assert Message.cast("sent")      == {:ok, :sent}
