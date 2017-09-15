dcd2324186cabfd51fac43ecccf74e27bcd8cb7a
Jung hunsoo <hunsoo.jung@buenaworks.com>
2017-09-16 04:58:11 +0900

    Remove empty spaces.

--- lib/exnumerable.ex
+++ lib/exnumerable.ex
@@ -2,7 +2,7 @@ defmodule Exnumerator do
   defmacro __using__(opts) do
     quote location: :keep, bind_quoted: [values: opts[:values]] do
       @behaviour Ecto.Type
-      
+
       def type,   do: :string
       def values, do: unquote(values)
       def sample, do: unquote(values) |> Enum.random
@@ -12,7 +12,7 @@ defmodule Exnumerator do
         def load(unquote(value)), do: {:ok, unquote(value)}
         def dump(unquote(value)), do: {:ok, unquote(value)}
       end
-      
+
       def cast(_), do: :error
       def load(_), do: :error
       def dump(_), do: :error
