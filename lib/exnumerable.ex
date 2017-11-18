be892fcf8e8922f85b5fb9b2b2d740fa27d261c2
Marcos Benatti Lauer <benatti77@gmail.com>
2017-11-17 23:56:46 -0200

    string support

--- lib/exnumerable.ex
+++ lib/exnumerable.ex
@@ -7,10 +7,18 @@ defmodule Exnumerator do
       def values, do: unquote(values)
       def sample, do: unquote(values) |> Enum.random
 
-      for value <- values do
-        def cast(unquote(value)), do: {:ok, unquote(value)}
-        def load(unquote(value)), do: {:ok, unquote(value)}
-        def dump(unquote(value)), do: {:ok, unquote(value)}
+      if Enum.all?(values, &(is_atom(&1))) do
+        for value <- values, atom = value, string = Atom.to_string(value) do
+          def cast(unquote(atom)),   do: {:ok, unquote(string)}
+          def load(unquote(atom)), do: {:ok, unquote(string)}
+          def dump(unquote(atom)),   do: {:ok, unquote(string)}
+        end
+      else
+        for value <- values do
+          def cast(unquote(value)), do: {:ok, unquote(value)}
+          def load(unquote(value)), do: {:ok, unquote(value)}
+          def dump(unquote(value)), do: {:ok, unquote(value)}
+        end
       end
 
       def cast(_), do: :error
